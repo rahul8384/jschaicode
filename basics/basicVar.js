eb2ae3feac7f37dbb5dae73ac3bbaaf4e5a779ea
rahul8384 <91208315+rahul8384@users.noreply.github.com>
2024-01-17 03:55:13 +0000

    changes

---
+++ basics/basicVar.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "rahul@gmail.com"
+var accountPass = "12345"
+accountCity = "Delhi"
+
+//accountId = 2
+accountEmail = "aman@gmail.com"
+accountPass = "31241"
+accountCity = "jaipur"
+console.table([accountId, accountEmail, accountPass, accountCity]);
