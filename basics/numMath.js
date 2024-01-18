0e3a3de737899bbdc42737dffb54b89e9a348c61
rahul8384 <91208315+rahul8384@users.noreply.github.com>
2024-01-18 03:46:27 +0000

    math and number

---
+++ basics/numMath.js
@@ -0,0 +1,9 @@
+const score = 400
+const balance = new Number(100)
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+const max = 6
+const min = 1
+console.log(Math.ceil(Math.random() * (max - min + 1)))
